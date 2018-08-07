f1239ee8a03277684e54e0f3953c7da5fb1ce29a
hassnsalem <spuhnbnj@gmail.com>
2018-08-07 14:56:23 -0400

    Third Commit

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
-const usernameElement = document.getElementById("username");
-const messageElement = document.getElementById("message");
+let usernameElement = document.getElementById("username");
+let messageElement = document.getElementById("message");
 const button = document.getElementById("Submit");
 button.addEventListener("click", updateDB);
-
+const login = document.getElementById("Login");
+login.addEventListener("click", updateDB)
 //create database
 const database = firebase.database().ref();
+
+let usernameLoginElement = document.getElementById("loginInput1");
+let passwordLoginElement = document.getElementById("loginInput2");
+const loginButton = document.getElementById("Login");
+loginButton.addEventListener("click", updateDB);
 
 //function to take place on "click"
 function updateDB(event) {
@@ -12,8 +18,8 @@ function updateDB(event) {
     const username = usernameElement.value;
     const message = messageElement.value;
     if (username !== "" & message !== ""){
-        usernameElement = ""
-        messageElement = ""
+        usernameElement.value = ""
+        messageElement.value = ""
 
         //make myJson to then push into the left_container
         let myJson = {
@@ -24,3 +30,73 @@ function updateDB(event) {
         database.push(myJson);
     }
 }
+
+database.on("child_added", addMessageToBoard);
+
+function addMessageToBoard(rowData) {
+    const data = rowData.val();
+    //display name:message
+    const name = data.USERNAME;
+    const message = data.MESSAGE;
+    
+    let postDiv = document.createElement("div");
+    let nameDiv = document.createElement("div");
+    let messageDiv = document.createElement("div");
+    let left_container = document.getElementById("left_container");
+    left_container.appendChild(postDiv);
+    postDiv.appendChild(nameDiv);
+    postDiv.appendChild(messageDiv);
+    nameDiv.innerHTML = name;
+    messageDiv.innerHTML = message;
+    postDiv.setAttribute("class", "postDiv");
+    nameDiv.setAttribute("class", "nameDiv" );
+    messageDiv.setAttribute("class", "messageDiv" );
+}
+
+/*
+let usernameLoginElement = document.getElementById("loginInput1");
+let passwordLoginElement = document.getElementById("loginInput2");
+const button = document.getElementById("Login");
+button.addEventListener("click", updateDB);
+const login = document.getElementById("Login");
+login.addEventListener("click", updateDB)
+//create database
+const database = firebase.database().ref();
+
+//function to take place on "click"
+function updateDB(event) {
+    event.preventDefault();
+    const username = usernameElement.value;
+    const message = messageElement.value;
+    if (username !== "" & message !== ""){
+        usernameElement.value = ""
+        messageElement.value = ""
+
+        //make myJson to then push into the left_container
+        let myJson = {
+            USERNAME : username,
+            MESSAGE : message
+        }
+        //push myJson to left_container
+        database.push(myJson);
+    }
+}
+
+database.on("child_added", addMessageToBoard);
+
+function addMessageToBoard(rowData) {
+    const data = rowData.val();
+    //display name:message
+    const name = data.USERNAME;
+    const message = data.MESSAGE;
+    let postDiv = document.createElement("div");
+    let nameDiv = document.createElement("div");
+    let messageDiv = document.createElement("div");
+    let left_container = document.getElementById("left_container");
+    left_container.appendChild(postDiv);
+    postDiv.appendChild(nameDiv);
+    postDiv.appendChild(messageDiv);
+    nameDiv.innerHTML = name;
+    messageDiv.innerHTML = message;
+}
+*/
